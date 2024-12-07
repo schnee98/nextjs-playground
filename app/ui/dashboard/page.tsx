f5a7175f29791887c2f2fb62789eede663b93634
schnee98 <jooeun06161@gmail.com>
2024-12-07 23:53:37 +0900

    refactor: avoid waterfall, fetch parallel

--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -1,16 +1,15 @@
 import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import {
-  fetchCardData,
-  fetchLatestInvoices,
-  fetchRevenue,
-} from "@/app/lib/data";
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "@/app/lib/data";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+  const [revenue, latestInvoices, cardData] = await Promise.all([
+    fetchRevenue(),
+    fetchLatestInvoices(),
+    fetchCardData(),
+  ]);
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = cardData;
 
   return (
     <main>
